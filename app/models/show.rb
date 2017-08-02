0434ecfdfb28e4673d609700ffc63b7acf04845c
linkcoding <kenny.lin.91@gmail.com>
2017-08-01 22:14:05 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    new_network = Network.create(call_letters: call_letters)
+    new_network.shows << self
+    #binding.pry
+  end
 end
